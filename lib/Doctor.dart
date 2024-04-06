62f7b7e1c1f56222b477a080efc68a32f79f2012
Nguyen-Duy-Son <nguyenduyson28072003@gmail.com>
2024-04-06 15:47:08 +0700

    update flutter-base

---
+++ lib/Doctor.dart
@@ -0,0 +1,21 @@
+class Doctor {
+  final String doctorId;
+  final String avatar;
+  final String name;
+  final String gender;
+  final String degree;
+  final int experience;
+  final String description;
+  final String departmentId;
+
+  const Doctor({
+    required this.doctorId,
+    required this.avatar,
+    required this.name,
+    required this.gender,
+    required this.degree,
+    required this.experience,
+    required this.description,
+    required this.departmentId,
+  });
+}
