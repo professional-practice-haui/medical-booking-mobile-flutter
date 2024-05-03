8a4cdb4038c930cf3d04ff42e326ad25032e712d
Nguyen-Duy-Son <nguyenduyson28072003@gmail.com>
2024-05-03 21:58:27 +0700

    test api

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:medical_booking_app/routes/routeGenrator.dart';
 import 'package:medical_booking_app/routes/routes.dart';
-void main() async{
-  await GetStorage.init();
+
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        initialRoute: RoutesWidget.routeHome,
-        onGenerateRoute: RouteGenerator.generateRoute,
+    return const MaterialApp(
+      initialRoute: RoutesWidget.routeHome,
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
